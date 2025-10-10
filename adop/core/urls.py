5f7f14589f5f5bf2be70576833e3673a0c38fbb2
JovannyLobato <jovannylobato2004@gmail.com>
2025-10-09 23:08:48 -0600

    agruege la vista detalle

--- adop/core/urls.py
+++ adop/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import index
+from . import views
 
 
 urlpatterns = [
     path('', index),
+    path('mascota/<int:id>/', views.detalle_mascota, name='detalle_mascota'),
 ]
--- adop/core/views.py
+++ adop/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Mascota
 
 
@@ -8,3 +8,7 @@ def index(request):
     mascotas = Mascota.objects.all()
     context = {'mascotas': mascotas}
     return render(request, template_name,context)
+
+def detalle_mascota(request, id):
+    mascota = get_object_or_404(Mascota, id=id)
+    return render(request, 'detalle.html', {'mascota': mascota})
