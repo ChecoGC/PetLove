045f688d0a4b710f935bba0e76761c24f5830f00
JovannyLobato <jovannylobato2004@gmail.com>
2025-10-12 20:18:34 -0600

    configura Django para usar MySQL en lugar de SQLite

--- adop/adop/settings.py
+++ adop/adop/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'adop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#'ENGINE': 'django.db.backends.sqlite3',
+#'NAME': BASE_DIR / 'db.sqlite3',
+        
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'petlove',
+        'USER': 'jovannylg',
+        'PASSWORD': 'lupita123',
+        'HOST': 'localhost',  
+        'PORT': '3306',
     }
 }
 
